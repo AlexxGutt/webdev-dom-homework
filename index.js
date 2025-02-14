038277cc86e121d0f7381511b15d5470bc6d6065
AlexxGutt <alex.gutt89@gmail.com>
2025-02-14 22:33:22 +0300

    process v.1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { renderUserComments } from './modules/renderComments.js'
 import { addComment } from './modules/initListeners.js'
 
+fetch('https://wedev-api.sky.pro/api/v1/alexxgutt/comments')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    })
 renderUserComments()
 addComment()
